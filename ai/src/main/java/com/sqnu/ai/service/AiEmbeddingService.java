bbf2bb450c0ab88c22b520b040c7cfb0740f32ff
lijinhui-456 <3012905795@qq.com>
2026-06-22 09:44:24 +0800

    Delete ai directory

--- ai/src/main/java/com/sqnu/ai/service/AiEmbeddingService.java
+++
@@ -1,9 +0,0 @@
-package com.sqnu.ai.service;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface AiEmbeddingService {
-    public void add() throws IOException;
-    public List<String> search(String query);
-}
